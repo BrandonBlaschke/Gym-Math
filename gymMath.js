780639ed9a6c4696dd9c1663aaa19ebf3301a16a
Brandon Blaschke <brandonj.blaschke@gmail.com>
2018-03-17 00:23:33 -0700

    Support for enter key

--- gymMath.js
+++ gymMath.js
@@ -1,36 +1,45 @@
-//Max Weight
-let max
-
 $(document).ready(function() {
 
   //Get Input for max weight
   $("#button").click(function() {
-    max = document.getElementById("MaxWeight").value
-    max = Number(max) * .9;
-
-    //Check if number is negative 
-    if (max < 0) {
-      alert("Number can't be < 0");
-    } else {
-
-      //Round down to nearest 5
-      max = Math.floor(max/5)*5;
-
-      //Display
-      $("#NewWeight").text("" + max);
-
-      //Calculate for the other %s
-      for(let i = 95; i >= 65; i-=5) {
-
-        let string = "#" + i + "Weight";
-        let newWeight = max * (i * .01);
-        newWeight = Math.floor(newWeight/5)*5;
-        $(string).text("" + newWeight);
-      }
-    }
-
-
+    let num = document.getElementById("MaxWeight").value
+    num = Number(num) * .9;
+    setNewWeights(num);
   });
 
+  //Enter Key check
+  $("#MaxWeight").keypress(function(e) {
+    let key = e.which;
+    if (key == 13) {
+      let num = document.getElementById("MaxWeight").value
+      num = Number(num) * .9;
+      setNewWeights(num);
+    }
+  })
 
 });
+
+//Sets the DOM elements to there weights
+function setNewWeights(max) {
+  //Check if number is negative
+  if (max < 0) {
+    alert("Number can't be < 0");
+  } else {
+
+    //Round down to nearest 5
+    max = Math.floor(max/5)*5;
+
+    //Display
+    $("#NewWeight").text("" + max);
+
+    //Calculate for the other %s
+    for(let i = 95; i >= 65; i-=5) {
+
+      let string = "#" + i + "Weight";
+      let newWeight = max * (i * .01);
+      newWeight = Math.floor(newWeight/5)*5;
+      $(string).text("" + newWeight);
+    }
+  }
+
+}
